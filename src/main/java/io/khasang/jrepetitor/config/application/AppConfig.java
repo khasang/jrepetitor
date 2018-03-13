2673b41ee47a40697c81dc28f9b2e636e19dd3e3
rtushkan <xseliot@mail.ru>
2018-03-13 18:02:22 +0300

    A cat created and named

---
+++ src/main/java/io/khasang/jrepetitor/config/application/AppConfig.java
@@ -0,0 +1,13 @@
+package io.khasang.jrepetitor.config.application;
+
+import io.khasang.jrepetitor.model.Cat;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public Cat cat(){
+        return new Cat("Barsik");
+    }
+}
--- src/main/java/io/khasang/jrepetitor/controller/AppController.java
+++ src/main/java/io/khasang/jrepetitor/controller/AppController.java
@@ -1,12 +1,18 @@
 package io.khasang.jrepetitor.controller;
 
+import io.khasang.jrepetitor.model.Cat;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class AppController {
+    @Autowired
+    private  Cat cat;
     @RequestMapping("/")
-    public String helloPage(){
+    public String helloPage(Model model){
+        model.addAttribute("name",cat.getName());
         return "hello";
     }
 }
