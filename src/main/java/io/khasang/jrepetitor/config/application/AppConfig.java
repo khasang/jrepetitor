299f0ece8f754fa505a7ba31d446810a7cee226d
skuznet <azon.sk@gmail.com>
2018-03-15 20:57:34 +0300

    add spring jdbc

--- src/main/java/io/khasang/jrepetitor/config/application/AppConfig.java
+++ src/main/java/io/khasang/jrepetitor/config/application/AppConfig.java
@@ -1,13 +1,47 @@
 package io.khasang.jrepetitor.config.application;
 
 import io.khasang.jrepetitor.model.Cat;
+import io.khasang.jrepetitor.model.CreateTable;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @Configuration
+@PropertySource(value = "classpath:util.properties")
 public class AppConfig {
+    @Autowired
+    private Environment environment;
+
     @Bean
     public Cat cat(){
         return new Cat("Barsik");
     }
+
+    @Bean
+    public DriverManagerDataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(environment.getRequiredProperty("jdbc.postgresql.driver"));
+        dataSource.setUrl("jdbc.postgresql.url");
+        dataSource.setUsername("jdbc.postgresql.user");
+        dataSource.setPassword("jdbc.postgresql.password");
+        return dataSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(){
+       JdbcTemplate jdbcTemplate = new JdbcTemplate();
+       jdbcTemplate.setDataSource(dataSource());
+       return jdbcTemplate;
+    }
+
+    @Bean
+    public CreateTable createTable(){
+        return new CreateTable(jdbcTemplate());
+    }
+
 }
--- src/main/java/io/khasang/jrepetitor/controller/AppController.java
+++ src/main/java/io/khasang/jrepetitor/controller/AppController.java
@@ -1,22 +1,32 @@
 package io.khasang.jrepetitor.controller;
 
 import io.khasang.jrepetitor.model.Cat;
+import io.khasang.jrepetitor.model.CreateTable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class AppController {
     @Autowired
-
     private Cat cat;
+    @Autowired
+    private CreateTable createTable;
 
     // Фабричный метод бинов.
     // Cat cat = new Cat();
 
+//    @RequestMapping("/")
+//    public String helloPage(){
+//        System.out.println(cat.getName());
+//        return "hello";
+//    }
+
     @RequestMapping("/")
-    public String helloPage(){
-        System.out.println(cat.getName());
-        return "hello";
+    public String createTableStatus(Model model) {
+        model.addAttribute("status", createTable.createTableStatus());
+        return "create";
     }
+
 }
