665cf4beb9fb38112ab154d898f4490a71a7e048
AlenaBK <alena@morko.ru>
2018-03-18 21:43:16 +0300

    add spring core

--- src/main/java/io/khasang/jrepetitor/controller/AppController.java
+++ src/main/java/io/khasang/jrepetitor/controller/AppController.java
@@ -1,12 +1,22 @@
 package io.khasang.jrepetitor.controller;
 
+import io.khasang.jrepetitor.model.Cat;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 @Controller
 public class AppController {
+    @Autowired
+    private Cat cat;
+
     @RequestMapping("/")
-    public String helloPage(){
+    public String helloPage(Model model) {
+        model.addAttribute("name", cat.getName());
         return "hello";
     }
 }
