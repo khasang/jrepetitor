d81cb89a1c62ca229a9e89735491c0d96f151d0d
DenisS <d_savrasov@rambler.ru>
2018-03-20 00:01:57 +0300

    Spring Security

---
+++ src/main/java/io/khasang/jrepetitor/config/application/SpringInit.java
@@ -0,0 +1,7 @@
+package io.khasang.jrepetitor.config.application;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SpringInit extends AbstractSecurityWebApplicationInitializer {
+
+}
