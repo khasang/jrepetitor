9226be9385203cba05820f7ee8249e3d18c02849
roman <rumyantsevro@gmail.com>
2018-06-05 14:06:10 +0300

    empty line removed

--- src/main/java/io/khasang/jrepetitor/controller/RightAnsController.java
+++ src/main/java/io/khasang/jrepetitor/controller/RightAnsController.java
@@ -17,21 +17,18 @@ public class RightAnsController {
     @RequestMapping(value = "/add", method = RequestMethod.POST, produces = {"application/json;charset=utf-8"})
     @ResponseBody
     public RightAns addRightAns(@RequestBody RightAns rightAns) {
-
         return rightAnsService.addRightAns(rightAns);
     }
 
     @RequestMapping(value = "/all", method = RequestMethod.GET, produces = "application/json;charset=utf-8")
     @ResponseBody
     public List<RightAns> getAllItemRightAns() {
-
         return rightAnsService.getAllRightAns();
     }
 
     @RequestMapping(value = "/get/{id}", method = RequestMethod.GET, produces = "application/json;charset=utf-8")
     @ResponseBody
     public RightAns getItemRightAnsById(@PathVariable(value = "id") String id) {
-        // exception
         return rightAnsService.getRightAnsById(Long.parseLong(id));
     }
 }
